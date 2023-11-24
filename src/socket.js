40d0f167dc28541b0fc7ac35af33775e8dc66d87
SUMANTHTPs <sumanthtps@gmail.com>
2023-11-25 00:36:16 +0530

    url updated

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,11 @@
 import io from "socket.io-client"; // Add this
+import { BASE_URL } from "./config";
+
 let socket;
+
 const connectSocket = (userId) => {
-  socket = io("http://localhost:5000", {
-    query: `userId=${window.localStorage.getItem("userId")}`,
+  socket = io(BASE_URL, {
+    query: `userId=${userId}`,
   });
 }; // Add this -- our server will run on port 4000, so we connect to it from here
 
